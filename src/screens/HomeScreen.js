b0d8a90d21b6fde1a20c29d705580608f683a487
abc <patelvimi22@gmail.com>
2023-08-25 15:30:22 +0530

    second changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity , StyleSheet } from 'react-native'
 import React from 'react'
 
 const  HomeScreen=()=> {
+  console.log("Home screen ")
   return (
     <View style={styles.Main}>
       <Text>HomeScreen</Text>
