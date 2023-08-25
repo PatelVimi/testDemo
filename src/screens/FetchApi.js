dd6ba6ead00191f6bc9631ebc3f4274914bae72c
aaabc <patelvimi22@gmail.com>
2023-08-25 16:02:29 +0530

    test-commit

--- src/screens/FetchApi.js
+++ src/screens/FetchApi.js
@@ -8,7 +8,6 @@ const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const FetchApi = () => {
-
   console.log("fetch screen")
 const Dispatch=useDispatch()
 const ApiData= useSelector((state)=>state?.FetchApi?.data)
